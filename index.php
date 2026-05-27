e6a940369f855bfc6fd022edf4e7c95fd8c4978b
Mirthe Valentijn <me@mirthe.org>
2026-05-27 21:00:07 +0200

    Kirby caching

--- index.php
+++ index.php
@@ -4,60 +4,81 @@
     ],
     'tags' => [
         'tvshowblock' => [
-            'attr' =>[
+            'attr' => [
                 'tmdb'
             ],
             'html' => function($tag) {
-
                 $tmdbid = $tag->tmdb;
                 $api_key = option('themoviedb.apiKey');
-               
-                $url = "https://api.themoviedb.org/3/tv/". $tmdbid ."?api_key=" . $api_key;
-                $ch = curl_init($url);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_USERAGENT, kirby()->site()->title());
-                $rawdata = curl_exec($ch);
-                curl_close($ch);
-                $movieinfo = json_decode($rawdata,true);
-                // print_r($movieinfo); exit(); 
 
-                $url = "https://api.themoviedb.org/3/tv/". $tmdbid ."/credits?api_key=" . $api_key;
-                $ch = curl_init($url);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                $rawdata_credits = curl_exec($ch);
-                curl_close($ch);
-                $credits = json_decode($rawdata_credits,true);
-                
+                $cache = kirby()->cache('mirthe.tvshowblock');
+                $cacheKey = 'tmdb-tv-' . $tmdbid;
+                $showData = $cache->get($cacheKey);
+
+                if ($showData === null) {
+                    $showUrl = "https://api.themoviedb.org/3/tv/" . $tmdbid . "?api_key=" . $api_key;
+                    $ch = curl_init($showUrl);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($ch, CURLOPT_USERAGENT, kirby()->site()->title());
+                    $rawShow = curl_exec($ch);
+                    curl_close($ch);
+
+                    $creditsUrl = "https://api.themoviedb.org/3/tv/" . $tmdbid . "/credits?api_key=" . $api_key;
+                    $ch = curl_init($creditsUrl);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($ch, CURLOPT_USERAGENT, kirby()->site()->title());
+                    $rawCredits = curl_exec($ch);
+                    curl_close($ch);
+
+                    $showData = [
+                        'show'    => json_decode($rawShow, true),
+                        'credits' => json_decode($rawCredits, true)
+                    ];
+                    $cache->set($cacheKey, $showData, 604800);
+                }
+
+                $movieinfo = $showData['show'] ?? null;
+                $credits = $showData['credits'] ?? null;
+
+                if (empty($movieinfo) || !is_array($movieinfo) || isset($movieinfo['status_code'])) {
+                    return '<div class="well"><div class="well-body">TV-show niet gevonden</div></div>';
+                }
+
+                $poster = isset($movieinfo['poster_path']) ? 'https://www.themoviedb.org/t/p/w200/'.$movieinfo['poster_path'] : '';
                 $mijnoutput = '<div class="well">';
-                $mijnoutput .= '<div class="well-img"><img src="https://www.themoviedb.org/t/p/w200/'.$movieinfo['poster_path'].'" alt=""></div>';
-                // show first season poster, if available. might add this as an option..
-                // $mijnoutput .= '<div class="well-img"><img src="https://www.themoviedb.org/t/p/w200/'.$movieinfo['seasons'][0]['poster_path'].'" alt=""></div>';
+                if ($poster !== '') {
+                    $mijnoutput .= '<div class="well-img"><img src="'.$poster.'" alt=""></div>';
+                }
                 $mijnoutput .= '<div class="well-body">';
-                $mijnoutput .= '<p><a href="https://www.themoviedb.org/tv/'.$movieinfo['id'].'">'.$movieinfo['name']."</a><br>
-                ". $movieinfo['first_air_date'].", ".$movieinfo['status'].", ".$movieinfo['number_of_seasons']." seasons</p>";
-                $mijnoutput .= '<p><em>'.$movieinfo['tagline']."</em></p>";
-                $mijnoutput .= '<p>'.mb_strimwidth($movieinfo['overview'],0,300, '&#8230;')."</p>";
+                $mijnoutput .= '<p><a href="https://www.themoviedb.org/tv/'.$movieinfo['id'].'">'.htmlspecialchars($movieinfo['name'] ?? '', ENT_QUOTES).'</a><br>';
+                $mijnoutput .= htmlspecialchars($movieinfo['first_air_date'] ?? '', ENT_QUOTES);
+                $mijnoutput .= ', '.htmlspecialchars($movieinfo['status'] ?? '', ENT_QUOTES);
+                $mijnoutput .= ', '.htmlspecialchars($movieinfo['number_of_seasons'] ?? '', ENT_QUOTES).' seasons</p>';
+                $mijnoutput .= '<p><em>'.htmlspecialchars($movieinfo['tagline'] ?? '', ENT_QUOTES).'</em></p>';
+                $mijnoutput .= '<p>'.mb_strimwidth($movieinfo['overview'] ?? '', 0, 300, '&#8230;').'</p>';
 
-                $i = 0;
-                $mijnoutput .= "<ul class=\"cast\">";
-                foreach ($credits['cast'] as $genre) {
-                    $mijnoutput .= '<li>'. $genre['name'] . "</li>";
-                    if (++$i == 7) break;
+                $mijnoutput .= '<ul class="cast">';
+                if (!empty($credits['cast']) && is_array($credits['cast'])) {
+                    $i = 0;
+                    foreach ($credits['cast'] as $genre) {
+                        $mijnoutput .= '<li>'.htmlspecialchars($genre['name'] ?? '', ENT_QUOTES).'</li>';
+                        if (++$i === 7) break;
+                    }
                 }
-                $mijnoutput .= "</ul>";
+                $mijnoutput .= '</ul>';
 
-                $mijnoutput .= "<ul class=\"genres\">";
-                foreach ($movieinfo['genres'] as $genre) {
-                    $mijnoutput .= '<li>'. $genre['name'] . "</li>";
+                $mijnoutput .= '<ul class="genres">';
+                if (!empty($movieinfo['genres']) && is_array($movieinfo['genres'])) {
+                    foreach ($movieinfo['genres'] as $genre) {
+                        $mijnoutput .= '<li>'.htmlspecialchars($genre['name'] ?? '', ENT_QUOTES).'</li>';
+                    }
                 }
-                $mijnoutput .= "</ul>";
+                $mijnoutput .= '</ul>';
 
                 $mijnoutput .= '</div></div>';
-               
                 return $mijnoutput;
             }
         ]
     ]
 ]);
 
-?>
